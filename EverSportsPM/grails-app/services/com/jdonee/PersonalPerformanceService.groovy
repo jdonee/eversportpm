01ca7d73b3581a6780ef199c33baed56fb41adfb
Jdonee <jdonee.zeng@gmail.com>
2011-11-08 16:17:20 +0800

    1,change PersonalPerformance module.

--- EverSportsPM/grails-app/services/com/jdonee/PersonalPerformanceService.groovy
+++ EverSportsPM/grails-app/services/com/jdonee/PersonalPerformanceService.groovy
@@ -1,6 +1,7 @@
 package com.jdonee
 
-import org.hibernate.FetchMode as FM
+import com.jdonee.utils.TemplateType
+import com.jdonee.utils.Constants
 
 class PersonalPerformanceService {
 
@@ -9,7 +10,7 @@ class PersonalPerformanceService {
 	def findAllPersonalPerformanceByUser(user,params) {
 		def results=[]
 		def jobs=Job.findAllByUserAndCompanyResponsible(user,Boolean.TRUE)
-		if(jobs.size()>0){
+		if(jobs){
 			results=PersonalPerformance.list(params)
 		}else{
 			def codes=Job.withCriteria{
@@ -27,5 +28,42 @@ class PersonalPerformanceService {
 			}
 		}
 		return results
+	}
+	
+	def initPersonalPerformance(personalPerformanceInstance){
+			if(personalPerformanceInstance.performance.useTemplate){
+			def jobContent=SystemTemplate.withCriteria(uniqueResult:true) {
+					projections{
+						property("templateContent")
+					}
+					eq('templateType', TemplateType.JOB)
+					eq("enabled",Boolean.TRUE)
+				}
+			if(jobContent){
+				def temps=jobContent.tokenize(Constants.DEFAULT_SEPARATOR)
+				if(temps){
+					temps.each{ value ->
+						 def jobRule=new JobRule(jobItem:value,personalPerformance:personalPerformanceInstance)
+						 jobRule.save(flush:true)
+				}
+				}
+			}
+			def companyContent=SystemTemplate.withCriteria(uniqueResult:true) {
+				projections{
+					property("templateContent")
+				}
+				eq('templateType', TemplateType.COMPANY)
+				eq("enabled",Boolean.TRUE)
+			}
+			if(companyContent){
+				def temps=companyContent.tokenize(Constants.DEFAULT_SEPARATOR)
+				if(temps){
+					temps.each{ value ->
+						 def companyRule= new CompanyRule(content:value,personalPerformance:personalPerformanceInstance)
+						 companyRule.save(flush:true)
+				}
+				}
+			}
+			}
 	}
 }
